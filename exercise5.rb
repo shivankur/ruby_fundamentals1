cf410aeb08d6f5638b1b59aecdcf2ee3e08069b5
Shiv <sharma.shivankur@gmail.com>
2017-06-26 16:41:07 -0400

    exercise5

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+puts "How many pizzas do you want to order?"
+pizza_quantity = gets.chomp.to_i
+
+current_pizza = 1
+
+while current_pizza <= pizza_quantity
+  puts "How many toppings for #{current_pizza}?"
+  pizza_toppings = gets.chomp.to_i
+  puts "You have ordered a pizza with #{pizza_toppings} toppings"
+  current_pizza += 1
+
+end
