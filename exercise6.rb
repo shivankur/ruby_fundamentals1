c76287712af8f618265f821068f6543226016cb6
Shiv <sharma.shivankur@gmail.com>
2017-06-26 18:02:26 -0400

    final

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,8 @@
+#EXERCISE 6.1 -----------------------------------------------------
 puts "Would you like to walk(1) or run(2)?"
-walk = 1
-run = 2
-stop = 3
-
+#walk = 1
+#run = 2
+#stop = 3
 
 answer = gets.chomp.to_i
 total_distance = 0
@@ -27,14 +27,31 @@ else
 
 end
   end
+#EXERCISE 6.2 -----------------------------------------------------
 
   energy = 100
+  while energy > 0
+  puts energy
   puts "Are you walking (1), running (2), resting (3), eating (4)"
   activity = gets.chomp.to_i
 
   if activity == 1
     energy += 5
 
-  end
+  elsif activity == 2
+    if energy >= 5
+      energy -=5
+    else
+      puts "Rest Up"
+    end
 
+  elsif activity == 3
+    energy +=1
+
+  elsif activity == 4
+      energy +=2
+
+
+  end
+end
   puts energy
