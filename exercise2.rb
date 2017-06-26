2900437249e1a502fa5f2ceb7b7d049bd9a682d5
Shiv <sharma.shivankur@gmail.com>
2017-06-26 14:52:39 -0400

    exercise3

--- exercise2.rb
+++ exercise2.rb
@@ -1,12 +1,17 @@
-#Calculating tip
+#Calculating tip for meal price $55
 
-meal = 55
-tip = meal.to_f * 0.25
+##tip = meal.to_f * 0.25
 
-puts "A good tip is #{tip}"
+#
+#Try adding a string and an integer with the + operator. What happens?
+#Find a way to convert the integer into a string first and use puts to print the result.
 
+#stringmeal = 55.to_s
+#
 
-#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
-#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+#puts "#{cost}"
+
 #Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
-#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+
+answer = 45628 * 7839
+puts "Two large integers multiplied together #{answer}"
