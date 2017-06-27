078bcbfe1823a895311f3487aff30ab7e2e0b504
Shiv <sharma.shivankur@gmail.com>
2017-06-26 22:27:14 -0400

    gorilla_update

--- exercise3.rb
+++ exercise3.rb
@@ -5,4 +5,4 @@
 puts "How old are you?"
 age = gets.chomp.to_i
 year = 2017 - age
-puts "You are born in #{year}"
+puts "You were born in #{year} , which makes you the smartest person on earth"
--- exercise5.rb
+++ exercise5.rb
@@ -6,7 +6,7 @@ current_pizza = 1
 while current_pizza <= pizza_quantity
   puts "How many toppings for #{current_pizza}?"
   pizza_toppings = gets.chomp.to_i
-  puts "You have ordered a pizza with #{pizza_toppings} toppings"
+  puts "You have ordered #{pizza_quantity} pizza(s) with #{pizza_toppings} toppings"
   current_pizza += 1
 
 end
